0563883732f0d2842e17d573303808f0954fdac8
Alex Pirogov <pirogovalexandr00@mail.ru>
2024-05-18 14:59:05 +0300

    feat: return to sha

--- core/git_processor.py
+++ core/git_processor.py
@@ -37,9 +37,9 @@ class GitProcessor[ConfigShapeT: GitProcessorConfigShape, TreeT, BlobT](metaclas
 
             setattr(cls, el, attr)
 
-    def __init__(self, config_dict: dict[str, Any], ref: str) -> None:
+    def __init__(self, config_dict: dict[str, Any], sha: str) -> None:
         self.config = self.validate_config(config_dict)
-        self.ref = ref
+        self.sha = sha
 
     def validate_config(self, config_dict: dict[str, Any]) -> ConfigShapeT:
         return self.config_shape.model_validate(config_dict)
